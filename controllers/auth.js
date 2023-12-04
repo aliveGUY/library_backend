0003aef17f3cfae31894a40feb5e6e32aa3f9b1d
illdub <illia.dubrovin@dreamhost.com>
2023-12-04 20:09:20 +0200

    update backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
 
 const login = async (req, res) => {
     const { username, password } = req.body
--- server.js
+++ server.js
@@ -1,35 +1,54 @@
 require('dotenv').config()
+require('express-async-errors')
 const express = require("express");
-const mongoose = require('mongoose')
+const app = express()
+const path = require('path')
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
-const cookieParser = require('cookie-parser')
-const app = express()
 const connectDB = require('./config/dbConn')
-const credentials = require('./middleware/credentials');
-const { logger } = require('./middleware/logger')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 4000;
 
+connectDB()
 
 // middleware
 app.use(logger)
-app.use(credentials)
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors(corsOptions))
-app.use(express.urlencoded({ extended: false }))
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 // routes
+app.use('/', require('./routes/root'))
 app.use('/auth', require('./routes/auth'))
 app.use('/books', require('./routes/books'))
 app.use('/users', require('./routes/users'))
-app.get('/', (req, res) => { res.json({ mssg: "welcome to the app" }) })
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
 
 // connect to db and listen
-connectDB()
 mongoose.connection.once('open', () => {
     app.listen(PORT, () =>
         console.log(`DB Connected\nPort Number: ${PORT}`)
     )
 })
 
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
